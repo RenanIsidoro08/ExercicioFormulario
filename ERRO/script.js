39667b377b676b903bc8f590c3fbaf349262f0d3
RenanIsidoro08 <renanisidoro08@gmail.com>
2024-05-31 17:07:05 -0300

    APAGAR PASTA ERRO

--- ERRO/script.js
+++
@@ -1,7 +0,0 @@
-var contador = 0;
-
-document.getElementById('incrementar').addEventListener('click', function() {
-    contador++;
-    document.getElementById('contador').textContent = 'Contador ='  + contador
-
-});
